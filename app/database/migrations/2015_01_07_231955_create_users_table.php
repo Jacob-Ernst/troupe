9a373b59421707094861600570d103e9d1a99545
Jacob Ernst <akumadude.je@gmail.com>
2015-01-08 11:59:56 -0600

    Created users and media resources. Updated the User model and added a Base model.

---
+++ app/database/migrations/2015_01_07_231955_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email', 200)->unique();
+		    $table->string('password', 255);
+		    $table->string('first_name', 255);
+		    $table->string('last_name', 255);
+		    $table->string('first_name_meta', 255)->index();
+		    $table->string('last_name_meta', 255)->index();
+		    $table->boolean('admin')->default(0);
+		    $table->softDeletes();
+		    $table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
---
+++ app/models/Medium.php
@@ -0,0 +1,5 @@
+<?php
+
+class Medium extends \Eloquent {
+	protected $fillable = [];
+}
