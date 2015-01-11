35a5c3f3bf7214cd7c9ebd021868053f2f809ce1
Jacob Ernst <akumadude.je@gmail.com>
2015-01-11 17:26:27 -0600

    Initial migration files for DB structuring.

---
+++ app/database/migrations/2015_01_09_201837_create_updates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUpdatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('updates', function(Blueprint $table)
+		{
+			$table->increments('id');
+		    $table->text('info');
+			$table->integer('updateable_id')->unsigned();
+			$table->string('updateable_type');
+		    $table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('updates');
+	}
+
+}
