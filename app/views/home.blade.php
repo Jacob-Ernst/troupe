ef8a3c5efa9608214015a63df08686c3de6b86fc
Jacob Ernst <akumadude.je@gmail.com>
2015-01-23 09:53:01 -0600

    Preliminary homepage and master blade

---
+++ app/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('header', "<title>Troupe</title>")
+
+@section('content')
+
+    <div class='row'>
+        <div class="col-md-4 col-md-offset-2">
+            <div class='container'>
+                <h1>Hello, world!</h1>
+                <p>...</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+        <div class="col-md-4 col-md-offset-2">
+            <div class='container'>
+                <h1>Hello, world!</h1>
+                <p>...</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+        
+        
+    </div>
+    <div class='container'>
+        <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>...</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+    </div>
+@stop
