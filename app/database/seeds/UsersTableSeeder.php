38ed67f8c6f0b2141b23131eed7eb29c4b66a996
Jacob Ernst <akumadude.je@gmail.com>
2015-01-26 11:27:33 -0600

    Updated and functional users table seeder.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -1,20 +1,18 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->delete();
 
-class UsersTableSeeder extends Seeder {
-
-	public function run()
-	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			User::create([
-
-			]);
-		}
-	}
+        $user = new User();
+        $user->email = 'admin@troupe.com';
+        $user->password = 'test';
+        $user->first_name = 'Jacob';
+        $user->last_name = 'Ernst';
+        $user->role = 'admin';
+        $user->save();
+    }
 
 }
