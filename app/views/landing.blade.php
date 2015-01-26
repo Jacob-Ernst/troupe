5fc22d2410b8b318441fdb8db47cd7974b622d0a
Jacob Ernst <akumadude.je@gmail.com>
2015-01-26 11:33:13 -0600

    basic home and landing pages

---
+++ app/views/landing.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('header', "<title>Troupe</title>")
+
+@section('content')
+
+    <div class="jumbotron magenta">
+        <h1>Welcome!</h1>
+        <div class="carousel">
+            <div class='text-center azure'><p>HI</p></div>
+            <div class='text-center blue'><img src="/img/squirtle.png"></div>
+            <div class='text-center green'><img src="/img/bulbasaur.png"></div>
+        </div>
+    </div>
+@stop
