68ea7e96c2bf9bd3ceb9a96b40cf8e04565aaf5b
Jacob Ernst <akumadude.je@gmail.com>
2015-02-03 14:22:11 -0600

    Updated the users table seeder to add DOB to the test user and to generate extra users for testing purposes

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -16,6 +16,8 @@ class UsersTableSeeder extends Seeder
         $user->last_name = 'Ernst';
         $user->role = 'admin';
         $user->type = 'director';
+        $user->gender = 'm';
+        $user->date_of_birth = '1994-12-27';
         $user->save();
         
         $faker = Faker::create();
@@ -26,7 +28,11 @@ class UsersTableSeeder extends Seeder
                 'first_name'      => $faker->firstName($gender = null),
                 'last_name'       => $faker->lastName,
                 'email'           => $faker->unique()->email,
-                
+                'password'        => Hash::make('test'),
+                'date_of_birth'   => $faker->date($format = 'Y-m-d', $max = '2006-00-00'),
+                'gender'          => $faker->randomElement($array = ['m','f', 'o', 'p']),
+                'type'            => $faker->randomElement($array = ['director','actor', 'writer', 'artist']),
+                'role'            => 'user'
             ]);
         }
     }
