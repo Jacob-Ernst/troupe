ab2cce807238879140a67e95d9e0bf9560779a54
Jacob Ernst <akumadude.je@gmail.com>
2015-02-17 14:25:14 -0600

    Added to basic home view for structural purposes.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,5 +7,47 @@
     <div class="jumbotron">
         <h1>home</h1>
     </div>
+    <div class="page-header">
+        <h1>Example page header <small>Subtext for header</small></h1>
+    </div>
+    <div class='row'>
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+        <div class="col-xs-6 col-lg-4 container">
+            <img class="img-square img-responsive" src="/img/bulbasaur.png" alt="Generic placeholder image">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!--/.col-xs-12.col-lg-4-->
+    </div>
+    <div class='row'></div>
 
 @stop
