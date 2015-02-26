6cfdd347621302b081f1690045a4784f1ce4ea77
Jacob Ernst <akumadude.je@gmail.com>
2015-02-26 13:35:34 -0600

    Updated the event modal form.

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -124,7 +124,7 @@
                                 
                             </div> 
                             <div class="modal-footer">
-                                {{Form::submit('Login', array('class' => 'btn btn-default'))}}
+                                {{Form::submit('Create', array('class' => 'btn btn-default'))}}
                             </div>
                             {{ Form::close() }}
                         </div><!-- /.modal-content -->
@@ -145,15 +145,79 @@
                                 <h3>Event</h3>
                             </div>
                             <div class="modal-body">
-                                <div class="input-group form-group">
-                                    {{ Form::label('email', 'Email:', array('class' => 'input-group-addon')) }}
-                                    {{ Form::text('email', Input::old('email') , array('class' => 'form-control')) }}
-                                </div>                        
-                                <div class="input-group form-group">
-                                    {{ Form::label('password', 'Password:', array('class' => 'input-group-addon')) }}
-                                    {{ Form::password('password', array('class' => 'form-control')) }}
+                                <div class='row'>
+                                <div class="input-group form-group col-xs-10 col-xs-offset-1">
+                                        {{ Form::label('title', 'Title:', array('class' => 'input-group-addon')) }}
+                                        {{ Form::text('title', Input::old('title') , array('class' => 'form-control')) }}
+                                    </div>
+                                    <div class="input-group form-group col-xs-10 col-xs-offset-1">
+                                        {{ Form::label('type', 'Type:', array('class' => 'input-group-addon')) }}
+                                        {{ Form::text('type', Input::old('type') , array('class' => 'form-control')) }}
+                                    </div>
+                                    <div class="input-group form-group col-xs-10 col-xs-offset-1">
+                                        {{ Form::label('summary', 'Summary:', array('class' => '')) }}
+                                        {{ Form::textarea('summary', null, ['class' => 'form-control']) }}
+                                    </div>
+                                    <div class="input-group form-group col-xs-10 col-xs-offset-1">
+                                        {{ Form::label('location', 'Location:', array('class' => '')) }}
+                                        {{ Form::textarea('location', null, ['class' => 'form-control']) }}
+                                    </div>
+                                    <div class='form-group'>
+                                        <div class='text-center'>
+                                            <h3>Date</h3>
+                                        </div>
+                                        <div class='col-xs-4'>
+                                            <div class='text-center'>
+                                                {{ Form::label('d_year', 'Year', array('class' => 'control-label')) }}
+                                            </div>
+                                            <select class="form-control" id="date-year" name="d_year" required>
+                                                
+                                                @for ($i=idate('Y'); $i <= (idate('Y') + 5); $i++)
+                                                    @if($i == idate('Y'))
+                                                        <option selected>{{{ $i }}}</option>
+                                                        
+                                                    @else
+                                                        <option>{{{ $i }}}</option>
+                                                    @endif
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class='col-xs-4'>
+                                            <div class='text-center'>
+                                                {{ Form::label('d_month', 'Month', array('class' => 'control-label')) }}
+                                            </div>
+                                            <select class="form-control" id="date-month" name="d_month" required>
+                                                <option>Month</option>
+                                                @for ($i=1; $i <= 12; $i++) 
+                                                    @if($i == idate('m'))
+                                                        <option selected>{{{ $i }}}</option>
+                                                        
+                                                    @else
+                                                        <option>{{{ $i }}}</option>
+                                                    @endif
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class='col-xs-4'>
+                                            <div class='text-center'>
+                                                {{ Form::label('d_day', 'Day', array('class' => 'control-label')) }}
+                                            </div>
+                                            <select class="form-control" id="date-day" name="d_day" required>
+                                                <option>Day</option>
+                                                @for ($i=1; $i <= 31; $i++) 
+                                                    @if($i == idate('d'))
+                                                        <option selected>{{{ $i }}}</option>
+                                                        
+                                                    @else
+                                                        <option>{{{ $i }}}</option>
+                                                    @endif
+                                                @endfor
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
                             </div> 
+                            <br>
                             <div class="modal-footer">
                                 {{Form::submit('Login', array('class' => 'btn btn-default'))}}
                             </div>
