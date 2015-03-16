33a089d164532d5e5febdca64ae8c4546ff2674e
Jacob Ernst <akumadude.je@gmail.com>
2015-03-16 11:04:07 -0500

    Add production type id to announcements migration

---
+++ app/database/migrations/2015_03_15_200328_add_production_type_id_to_announcements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddProductionTypeIdToAnnouncementsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('announcements', function(Blueprint $table)
+		{
+			$table->integer('production_type_id')->unsigned()->index();
+            $table->foreign('production_type_id')->references('id')->on('production_types')->onDelete('cascade');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('announcements', function(Blueprint $table)
+		{
+			$table->dropForeign('announcements_production_type_id_foreign');
+			$table->dropColumn('production_type_id');
+		});
+	}
+
+}
